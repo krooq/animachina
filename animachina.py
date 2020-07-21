9d8fb29e01df9126c421274faee414cde002719a
Gavin Rossiter <rossiter.gavin@gmail.com>
2020-07-21 10:24:22 +1000

    pretty

--- animachina.py
+++ animachina.py
@@ -11,39 +11,56 @@ from kivy.core.window import Window
 from array import array
 
 import numpy as np
+import time
+import sys
 
 rng = np.random.default_rng()
+f32 = np.float32
+u8 = np.uint8
+
+def fbounds(dtype):
+    ''' Bounds for a floating point data type. '''
+    return (np.finfo(f32).min, np.finfo(f32).max)
 
 def rgba(width=128, height=128):
-    return np.zeros([width, height, 4], dtype=np.uint8)
+    ''' RGBA array of zeros. '''
+    return np.zeros([width, height, 4], dtype=u8)
 
+def dist(a):
+    ''' Probability distribution array from parts. '''
+    return np.array(a)/np.sum(a)
 
 class World:
     def __init__(self, height, width):
-        self.environment = np.zeros((height, width), dtype=np.uint8)
+        self.size = (height, width)
+        self.environment = np.zeros(self.size, dtype=f32)
 
     def update(self):
         # select a batch of random cell's (maybe all?)
             # increase the selected cell's energy by some small amount
-            add = np.random.choice(np.array([0,1], dtype=np.uint8), self.environment.shape, p=[0.75, 0.25])
-            self.environment += add
+            add = np.random.choice(np.array([-1,0,1]), self.environment.shape, p=dist([1,4,1]))
+            add = add * rng.random(self.environment.shape, dtype=f32)
+            self.environment = np.array(np.add(add, self.environment).clip(0, np.finfo(f32).max), dtype=f32)
+
         # select a batch of random cell's (maybe all?)
             # decrease the selected cell's energy by some small amount
-            # rem = np.random.choice(np.array([0,1], dtype=np.uint8), *self.environment.shape, p=[0.75, 0.25])
-            # self.environment -= rem
+            # rem = np.random.choice(np.array([0,1], dtype=np.uint16), self.environment.shape, p=[0.75, 0.25])
+            # self.environment = np.array(np.subtract(self.environment, rem).clip(0,255), dtype=np.uint8)
 
         # select a batch of random cell's (maybe all?)
             # transfer a random % of that cell's energy to an adjacent cell
-            # tfr = np.random.choice([0,1], *self.environment.shape, p=[0.75, 0.25])
-            # tfr *= np.random.rand(*self.environment.shape)
-            # tfr *= self.environment
+            # tfr = np.random.choice([0,1], self.environment.shape, p=dist([10, 1]))
+            # tfr = tfr * rng.random(self.environment.shape, dtype=np.float32)
+            # tfr = tfr * self.environment
+            # print(tfr)
+            # self.environment = np.array(np.subtract(self.environment, tfr).clip(0,255), dtype=np.uint8)
             # self.environment -= tfr
             # mask = np.ma.masked_values(tfr, 0)
             # tfr = tfr[mask]
             # for (x, y), value in np.ndenumerate(tfr):
             #     dx = np.random.choice([-1,1], (1,1))
             #     s = rng.standard_normal(size=(1,1))
-                # if 0 <= x <= self.environment.shape[0]:
+            #     if 0 <= x <= self.environment.shape[0]:
 
 
 class TextureBuffer(Widget):
@@ -69,25 +86,62 @@ class Animachina(App):
 
     def __init__(self, **kwargs):
         super(Animachina, self).__init__(**kwargs)
-        self.world = World(128,128)
-        (h, w) = Window.size
-        self.pbuffer = rgba(*self.world.environment.shape)
+        self.frame_monitor = FrameMonitor()
+        self.world = World(256,256)
+        self.pbuffer = rgba(*self.world.size)
         self.view = None
+        # self._keyboard = Window.request_keyboard(self._keyboard_closed, self, 'text')
 
     def update(self, dt):
-        self.world.update()
-        (h, w) = Window.size
-        self.pbuffer = np.full((128,128, 4), [128,128,128,255], dtype=np.uint8)
-        self.pbuffer[:,:] = self.world.environment[:,:,None]
-        # self.pbuffer = self.world.environment[:,:]
-        print(self.pbuffer)
-        self.view.update(self.pbuffer, self.pbuffer.shape[:2], (h, w))
+        with self.frame_monitor:
+            self.world.update()
+            speed = 10
+            self.pbuffer = speed * np.full((*self.world.size, 4), [1,1,1,1], dtype=f32)
+            self.pbuffer[:,:] *= self.world.environment[:,:,None]
+            self.pbuffer = self.pbuffer.clip(0,255).astype(u8)
+            # print(self.pbuffer)
+            # self.pbuffer = self.world.environment[:,:]
+            # print(self.pbuffer)
+            self.view.update(self.pbuffer, self.pbuffer.shape[:2], Window.size)
+
 
     def build(self):
         self.view = TextureBuffer(self.pbuffer, self.pbuffer.shape[:2])
-        Clock.schedule_interval(self.update, 1.0 / 60.0)
+        Clock.schedule_interval(self.update, 0)
         return self.view
 
+    # def _keyboard_closed(self):
+    #     self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+    #     self._keyboard = None
+
+    # def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+    #     print('The key', keycode, 'have been pressed')
+    #     print(' - text is %r' % text)
+    #     print(' - modifiers are %r' % modifiers)
+
+    #     # Keycode is composed of an integer + a string
+    #     # If we hit escape, release the keyboard
+    #     if keycode[1] == 'escape':
+    #         keyboard.release()
+
+    #     # Return True to accept the key. Otherwise, it will be used by the system.
+    #     return True
+
+class FrameMonitor:
+    def __init__(self):
+        self.frame_time = 0
+        self.frame_number = 0
+
+    def __enter__(self):
+        self.frame_number += 1
+        self.frame_number %= 30
+        self.frame_start = time.time_ns()
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.frame_number == 0:
+            self.frame_time = (self.frame_time + (time.time_ns() - self.frame_start) / 1000000)/2
+            sys.stdout.write("frame_time = {:3.9f} ms\r".format(self.frame_time))
+            sys.stdout.flush()
 
 if __name__ == "__main__":
     Animachina().run()
