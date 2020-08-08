f2ec1120330c4b30253d17df150e693a49e0f4c7
Gavin Rossiter <rossiter.gavin@gmail.com>
2020-08-08 17:10:13 +1000

    indexing is crazy business

--- README.md
+++ README.md
@@ -5,7 +5,17 @@ From the latin anima + machina = life machine... I'm so edgy...
 Anyway this is just a place to practice my AI/ML and Python.
 
 ## Projects
-### Spiking Neuron
+### Spiking Neuron based AI system
+
+#### Architecture
+The end-to-end architecture for data flow is:
+- Environment
+- Sensors
+- Routers
+- Neurons
+- Routers
+- Actuators
+- Environment
 
 #### Neuron Model
 | parameter         | dtype | shape | variability | notes                                                                                           |
@@ -20,29 +30,41 @@ Anyway this is just a place to practice my AI/ML and Python.
 | sensitivity delta | f32   | ()    | static      | How much the *sensitivity* changes w.r.t. **input activation** and this neurons **activation**  |
 | transfer delta    | f32   | ()    | static      | How much *potential* is transferred to the *target* neuron when this neuron **activates**       |
 
+#### Signals
+
+##### Data signals
+Data signals are the interface type between hardware drivers and software.
+There are many formats but at some level they are all just streams of arrays of data.
+Examples include video data, audio data, instructions for mechanical operations etc.
+
+##### Spike signals
+Spike signals are the interface type between regular software and the AI system software.
+These signals have a simple format, usually just a stream of a single number to indicate the spike level.
+
 #### Spike Transducers
 Spiking Neurons rely on input signals that vary in the time domain.
-Spike transducers convert data signals from one format into a spike signal that the neuron can process.
-They also perform the reverse transformation from spike signals to some data format.
-As humans we are limited in our actions, we can only move our muscles, but this lets us do a lot of things, like write, speak, walk etc.
-Computers however struggle with motor functions but can do a lot of other things like create patterns in sounds or light.
-But to do these things we need to create a piece of software that can translate the task.
+Spike transducers convert *data signals* into *spike signals* that the neuron can process.
+They also perform the reverse transformation from *spike signals* to *data signals* that regular software can process.
 
-##### Sensor - Inputs
-Takes some data and increases the *potential* of some neurons in a localized region.
+##### Sensors
+Sensors are the inputs to the artificial intelligence system.
+
+Sensors take some data and increases the *potential* of some neurons in a localized region.
 The data can be encoded by any method that maps input data to neuron potential.
 e.g. For a text processor the letter "a" can be mapped to a single neuron, 
 if you wanted you could also map "ab" to a single neuron but then you would need to repeat this for "ac" and so on.
-For an visual processor, you would need to define a sensor size and downscale inputs to match, you would also need a neuron for each color.
+For an visual processor, you would need to define a sensor size and downscale inputs to match, you would also need a neuron for each color RGB.
 The alpha channel wouldn't need to be processed by the neuron but it may need to be blended with other images if processing raw data, it can be tricky.
 
-##### Actuator - Ouputs
-Takes the *potentials* of some neurons in a localized region and turns them into some data.
+##### Actuators
+Actuators are the outputs of the artificial intelligence system.
+
+Actuators take the *potentials* of some neurons in a localized region and turns them into some data.
 Again, any mapping from a single neuron to single piece of output data will work, just follow the reverse process of the sensors.
 
-#### Wiring
+#### Routers
 Ok so we receive a signal from a sensor, it is propogated through the neurons which store the pattern, then somewhere it emerges from another set of neurons, and then into an actuator to do a thing.
-But which neurons do we wire up our actuator to? It's not like we know where the signals will emerge.
+But which neurons do we wire up our actuator to?
 And what if we want to do many things using different actuators triggered by a single input signal?
 For these functions we need something that tells the neurons where to send their signals.
 In the brain this the routing of signals is hardwired (more or less), each section of the brain performs a different task and neurons are wired to send signals to the correct sections.
@@ -51,7 +73,9 @@ We will need to do some wiring of our own to get our signals to the right locati
 ##### Reward Pathway
 How can we get a thing to do a thing? This ability doesn't just emerge out of the above neuron model.
 One of the major features that defines intelligence is the ability to integrate information from multiple sources and react to it.
-
+This creates a feedback loop, but to make any kind of change to the model we need to have some sort of policy to adapt to the feedback.
+A policy is typically fixed in most ML algorithms as they are designed to achieve one single task, out model however will need to adapt its policy.
+In the brain it is thought that there is a dedicated systems e.g. "dopamine pathway" that regulates model changes.
 
 #### Investigation Topics
 ##### "Always-On" input signal
---
+++ v3.py
@@ -0,0 +1,212 @@
+import cv2
+import numpy as np
+import pyautogui
+from screeninfo import get_monitors
+from scipy.spatial.distance import pdist, squareform
+
+# # display screen resolution, get it from your OS settings
+# SCREEN_SIZE = (3840, 2160)
+# # define the codec
+# fourcc = cv2.VideoWriter_fourcc(*"XVID")
+# # create the video write object
+# out = cv2.VideoWriter("output.avi", fourcc, 20.0, (SCREEN_SIZE))
+
+# while True:
+#     # make a screenshot
+#     # img = pyautogui.screenshot()
+#     img = pyautogui.screenshot(region=(0, 0, 300, 400))
+#     # convert these pixels to a proper numpy array to work with OpenCV
+#     frame = np.array(img)
+#     # convert colors from BGR to RGB
+#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     # write the frame
+#     out.write(frame)
+#     # show the frame
+#     cv2.imshow("screenshot", frame)
+#     # if the user clicks q, it exits
+#     if cv2.waitKey(1) == ord("q"):
+#         break
+
+# # make sure everything is closed when exited
+# cv2.destroyAllWindows()
+# out.release()
+
+
+# Python program to take 
+# screenshots   
+
+
+
+def screen_capture(filename, region=None):
+    pm = get_monitors()[0]
+    (x,y,w,h) = region if region != None else (pm.x, pm.y, pm.width, pm.height)
+    print(str((x,y,w,h)))
+    # take screenshot using pyautogui 
+    image = pyautogui.screenshot(region=(x,y,w,h)) 
+
+    # since the pyautogui takes as a  
+    # PIL(pillow) and in RGB we need to  
+    # convert it to numpy array and BGR  
+    # so we can write it to the disk 
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR) 
+    # writing it to the disk using opencv 
+    cv2.imwrite(filename, image)
+
+# screen_capture("img.png", region=(0,0,32,23))
+
+# Q: Can artificial neurons self organize to capture spacial data from linearizd data?
+# i.e. if we input linearized data will the network automatically capture the 2D representation in their structure?
+# is it worth exploring or should we just do this manually? Probs not worth it....
+#
+# ALGORITHM: sensing an image
+# NOTE: this is only for sensing an image, 
+#       there will need to be an actuator algorithm to respond to the image, 
+#       perhaps this can be "look around" and we can validate by seeing the
+#       image change in memory (we will need some sort of probe)
+# - capture image as some typical image format
+# - map image to internal data format decoding the 2D information that encoded in the size and indices of the image
+# - convert to spike signals direct into sensor neurons
+# - push sensor signals into short term memory neurons that are highly elastic
+# - push short term memory into long term memory (in file?)
+# NOTE: there may be many levels of short-to-long term memory, but at some point it needs to move to file
+
+def image_sensor(pos=(0,0),size=(64,64)) -> np.ndarray:
+    (x,y) = pos
+    (w,h) = size
+    image = pyautogui.screenshot(region=(x,y,w,h))
+    image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+    return image
+
+def update_stm(signal: np.ndarray, stm, ltm):
+    # update the stm based on new signal and exisiting ltm and stm
+    pass
+
+def update_ltm(stm, ltm):
+    # update the ltm based on exisiting ltm and stm
+    pass
+
+def image_actuator(stm, ltm):
+    # apply some activation function to stm + ltm
+    # if activates:
+    #    move pos of image_sensor based on result
+    pass
+
+
+
+# simple neuron model
+# position  : (2,) [0,j],[0,k]      # position in a numpy array
+# baseline  : ()   [0,t]            # value of this neuron at rest                         
+# potential : ()   [0,t]            # current value (state)                                
+# threshold : ()   [n,1]            # value at which the neuron fires                      
+# affinities: (j,) [0,1]            # ease at which this neuron responds to input signals       
+# amplitude : ()   [0,1]            # how much this neuron affects downstream neurons
+# elasticity: ()   [0,1]            # rate at which plastified afinities decay without stimulation
+# plasticity: ()   [0,1]            # rate at which affinities change when stimulated                       
+# stability : ()   [0,1]            # resistance to potential decay i.e. affects potential
+
+
+class Defaults:
+    baseline=0
+    potential=0
+    threshold=0.5
+    affinities=1.0
+    # meta parameters, not strictly required
+    amplitude=1.0
+    elasticity=0.5
+    plasticity=0.5
+    stability=0.5
+
+# neuron update rule sketch
+# note: all of this should be performed as immutable update
+# 
+# for neurons `n` with inputs `i` and outputs `o`
+# 
+# if potential[n] > threshold[n]:
+#   potential[n] = baseline[n]                                                      # normally neurons have a refractory period, perhaps a refractory period can regulate repeated stimulation
+#   potential[o] += amplitude[n] * affinities[o][n]                                 # this should scale with number of input/output neurons
+#   affinities[n][i] *= (1 - plasticity[i]) * (1 + potential[o] - threshold[o])     # careful, will have to clamp to minimum or dropout and clamp max as well
+# 
+# affinities[n][i] *= (1 - elasticity[n])                                           # this one will require a lot of tuning
+# potential[n] *= (1 - stability[n])                                                # this one will require some tuning
+
+rng = np.random.default_rng()
+
+def reprnd(arr, name="arr"):
+    return "---------------------\n{}: {}\n{}\n".format(name, arr.shape, arr.__repr__())
+
+def repr3d(arr, name="arr"):
+    arrfmt = "\n\n".join(["z=" + str(z) + "\n" + arr[...,z].__repr__() for z in range(arr.shape[2])])
+    return "---------------------\n{}: {}\n{}\n".format(name, arr.shape, arrfmt)
+
+class Model:
+    def __init__(self, shape, nb_inputs=1, nb_outputs=1):
+        self.shape          = shape
+        self.baseline       = 0.0 * np.ones(self.shape)
+        self.potential      = 0.0 * np.ones(self.shape)
+        self.threshold      = 0.5 * np.ones(self.shape)
+        self.affinities     = 0.5 * np.ones(self.shape)
+        self.stability      = 0.5 * np.ones(self.shape)
+        indices = np.array([i for i in np.ndindex(shape)])
+        distances = squareform(pdist(indices, 'chebyshev'))
+        inputs = rng.choice(indices, (indices.shape[0], nb_inputs))
+        # list of index neighbourhoods of distance exactly 1
+        # e.g. index_neighbourhoods[0] is a list of indices that are exactly 1 away from indices[0] 
+        index_neighbourhoods = [indices[(distances[idx] - 1) == 0] for idx in range(indices.shape[0])]
+
+    def __repr__(self):
+        return "{}\n{}".format(
+            reprnd(self.potential, "potential"), 
+            reprnd(self.affinities, "affinities")
+        )
+
+    def asarray(self):
+        return [self.baseline, self.potential, self.threshold, self.affinities, self.stability]
+
+def update(model):
+    [baseline, potential, threshold, affinities, stability] = model.asarray()
+    new_potential = potential.copy()
+    new_affinities = affinities.copy()
+    # activation_mask = (potential > threshold).astype(int)
+    new_potential = potential.where(potential > threshold, baseline, potential)
+
+    if potential > threshold:
+        # new_potential[n] = baseline[n]                                                     
+        new_potential[o] += affinities[o][n]                            
+        new_affinities[n][i] *= (1 + potential[o] - threshold[o])
+
+    new_potential[n] *= (1 - stability[n])
+    return [new_potential, new_affinities]
+
+# distance metrics:
+#   chebyshev
+#   cityblock / manhattan
+
+def run():
+    model = Model((4,4), 1)
+    pass
+    # [baseline_0, potential_0, threshold_0, affinities_0, stability_0] = 
+    # model = Model(16, 2)
+    # print(model)
+    # baseline     = baseline_0.copy()
+    # potential    = potential_0.copy()
+    # threshold    = threshold_0.copy()
+    # affinities   = affinities_0.copy()
+    # stability    = stability_0.copy()
+    # for i in range(10):
+    #     [new_potential, new_affinities] = update(layer)
+    #     potential = new_potential
+    #     affinities = new_affinities
+
+run()
+
+# def dbg(fstring):
+#     print("---------------\n{}\n".format(fstring.replace("=","\n",1)))
+
+def dbg(thing):
+    print("---------------\n{}\n".format(fstring.replace("=","\n",1)))
+
+def ndbg(ndarray, label=""):
+    print(reprnd(ndarray, label))
+
+# cv2.imshow("img", image_sensor())
+# cv2.waitKey(0)
