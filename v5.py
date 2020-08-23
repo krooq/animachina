32f6cd4cb1a7b8d1b4d87629c4b0cda3e346e15f
Gavin Rossiter <rossiter.gavin@gmail.com>
2020-08-23 13:28:09 +1000

    way better

--- v5.py
+++ v5.py
@@ -5,33 +5,35 @@ from bindsnet.network.nodes import Input, LIFNodes, Nodes
 from bindsnet.network.topology import Connection
 from bindsnet.network.monitors import Monitor
 from bindsnet.analysis.plotting import plot_spikes, plot_voltages
-from collections import namedtuple
-from typing import Dict, Optional, Type, Iterable, Union, Sequence
+
+# Very high level API for bindsnet
+class Net(Network):
+    def add_layer(self, nodes: Nodes, name: str) -> str:
+        super().add_layer(nodes, name)
+        setattr(nodes, 'name', name)
+        return nodes
+
+    def add_connection(self, connection: Connection) -> Connection:
+        super().add_connection(connection, source=connection.source.name, target=connection.target.name)
+        return connection
+
+    def add_monitor(self, monitor: Monitor, name: str) -> Monitor:
+        super().add_monitor(monitor, name)
+        return monitor
 
 # Model
-layer_0 = Input(n=100)
-layer_1 = LIFNodes(n=1000)
-cxn_0_1 = Connection(layer_0, layer_1, w=0.05 + 0.1 * torch.randn(layer_0.n, layer_1.n))
-cxn_1_1 = Connection(layer_1, layer_1, w=0.025 * (torch.eye(layer_1.n) - 1))
-
-# Monitoring
 time = 500
-mon_0 = Monitor(layer_0, ("s",), time)
-mon_1 = Monitor(layer_1, ("s","v"), time)
+net = Net()
+layer_0 = net.add_layer(Input(n=100), 'layer_0')
+layer_1 = net.add_layer(LIFNodes(n=1000), 'layer_1')
+cxn_0_1 = net.add_connection(Connection(layer_0, layer_1, w=0.05 + 0.1 * torch.randn(layer_0.n, layer_1.n)))
+cxn_1_1 = net.add_connection(Connection(layer_1, layer_1, w=0.025 * (torch.eye(layer_1.n) - 1)))
+mon_0   = net.add_monitor(Monitor(layer_0, ("s",), time), 'mon_0')
+mon_1   = net.add_monitor(Monitor(layer_1, ("s","v"), time), 'mon_1')
 
-net = Network()
-net.add_layer(layer_0, 'layer_0')
-net.add_layer(layer_1, 'layer_1')
-net.add_connection(cxn_0_1, 'layer_0', 'layer_1')
-net.add_connection(cxn_1_1, 'layer_1', 'layer_1')
-net.add_monitor(mon_0, 'mon_0')
-net.add_monitor(mon_1, 'mon_1')
-
-# Create and assign input signals for each timestep
-input_data = torch.bernoulli(0.1 * torch.ones(time, layer_0.n)).byte()
-inputs = {'layer_0': input_data}
-
-# Simulate network on input data.
+# Create input signals for each timestep and assign to input layers
+inputs = {'layer_0': torch.bernoulli(0.1 * torch.ones(time, layer_0.n)).byte()}
+# Simulate network on input data
 net.run(inputs, 100)
 
 # Retrieve and plot simulation spike, voltage data from monitors.
