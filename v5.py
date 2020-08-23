cf782a0780324dbb88a9d7208a8e1bd83bc2794a
Gavin Rossiter <rossiter.gavin@gmail.com>
2020-08-23 21:58:48 +1000

    got-somewhere

--- v5.py
+++ v5.py
@@ -5,12 +5,17 @@ from bindsnet.network.nodes import Input, LIFNodes, Nodes
 from bindsnet.network.topology import Connection
 from bindsnet.network.monitors import Monitor
 from bindsnet.analysis.plotting import plot_spikes, plot_voltages
+from bindsnet.learning import MSTDP
+from bindsnet.pipeline import EnvironmentPipeline
+from bindsnet.encoding import bernoulli
+from bindsnet.environment import GymEnvironment
+from bindsnet.pipeline.action import select_softmax
 
 # Very high level API for bindsnet
 class Net(Network):
     def add_layer(self, nodes: Nodes, name: str) -> str:
         super().add_layer(nodes, name)
-        setattr(nodes, 'name', name)
+        nodes.name = name
         return nodes
 
     def add_connection(self, connection: Connection) -> Connection:
@@ -22,25 +27,41 @@ class Net(Network):
         return monitor
 
 # Model
-time = 500
 net = Net()
-layer_0 = net.add_layer(Input(n=100), 'layer_0')
-layer_1 = net.add_layer(LIFNodes(n=1000), 'layer_1')
-cxn_0_1 = net.add_connection(Connection(layer_0, layer_1, w=0.05 + 0.1 * torch.randn(layer_0.n, layer_1.n)))
-cxn_1_1 = net.add_connection(Connection(layer_1, layer_1, w=0.025 * (torch.eye(layer_1.n) - 1)))
-mon_0   = net.add_monitor(Monitor(layer_0, ("s",), time), 'mon_0')
-mon_1   = net.add_monitor(Monitor(layer_1, ("s","v"), time), 'mon_1')
+layer_0 = net.add_layer(Input(n=4, traces=True), 'layer_0')
+layer_1 = net.add_layer(LIFNodes(n=10, traces=True), 'layer_1')
+layer_2 = net.add_layer(LIFNodes(n=2, traces=True, refrac=0,), 'layer_2')
+cxn_0_1 = net.add_connection(Connection(layer_0, layer_1, wmin=0, wmax=1))
+cxn_1_2 = net.add_connection(Connection(layer_1, layer_2, wmin=0, wmax=1, update_rule=MSTDP, nu=1e-1, norm=0.5 * layer_1.n))
 
-# Create input signals for each timestep and assign to input layers
-inputs = {'layer_0': torch.bernoulli(0.1 * torch.ones(time, layer_0.n)).byte()}
-# Simulate network on input data
-net.run(inputs, 100)
+# Load the gym environment.
+env = GymEnvironment('CartPole-v0')
+env.reset()
 
-# Retrieve and plot simulation spike, voltage data from monitors.
-spikes = {'layer_0': mon_0.get("s"), 'layer_1': mon_1.get("s")}
-voltages = {'layer_1': mon_1.get("v")}
+# Build pipeline from specified components.
+pipeline = EnvironmentPipeline(
+    net,
+    env,
+    action_function=select_softmax,
+    num_episodes=100,
+    output=layer_2.name,
+    render_interval=1,
+    reward_delay=None,
+    time=1,
+)
 
-plt.ioff()
-plot_spikes(spikes)
-plot_voltages(voltages, plot_type="line")
-plt.show()
+for i in range(100):
+    total_reward = 0
+    pipeline.reset_state_variables()
+    is_done = False
+    while not is_done:
+        result = pipeline.env_step()
+        pipeline.step(result)
+
+        reward = result[1]
+        total_reward += reward
+
+        is_done = result[2]
+    print(f"Episode {i} total reward:{total_reward}")
+
+env.close()
